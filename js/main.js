68a9a0d961fe84899bc9333cb906b07cc9370b35
EasyPick <andrea.ballabio@it.abb.com>
2026-01-13 22:51:36 +0100

    PRIMA PROCA

---
+++ js/main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Sito matrimonio caricato");
+
+  // Qui in futuro:
+  // - animazioni leggere
+  // - validazioni
+  // - gestione form
+});
